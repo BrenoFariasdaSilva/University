ba0b8b003b5504232da8a6dc090704d59193f446
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2021-11-01 07:09:56 -0300

    Add files via upload

---
+++ Algorithm-and-Data-Structure-02/Week-01-SortingAlgorithms/QuickSort-Standard.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 50000
+
+int* randomVector(int n, int max, int seed);
+void troca (int* vector, int i, int j);
+int standardPartition (int* vector, int startIndex, int endIndex);
+void standardQuickSort (int* vector, int left, int right);
+
+int main (void)
+{
+	int* vector = randomVector (SIZE, SIZE * 100, 42);
+	standardQuickSort(vector, 0, SIZE - 1);
+
+	double start_t = clock();
+	standardQuickSort(vector, 0, SIZE - 1);
+	double end_t = clock();
+	double timeLapse = (end_t - start_t) / CLOCKS_PER_SEC;
+
+	printf("Time Lapse: %f \n", timeLapse);
+
+	return 0;
+}
+
+int* randomVector(int n, int max, int seed)
+{
+	int* vector = malloc(n * sizeof(int));
+
+	srand (seed);
+	for (int i = 0; i < SIZE; i++) 
+		vector[i] = rand () % max;
+
+	return vector;
+}
+
+void troca (int* vector, int i, int j)
+{
+	int hold = vector[i];
+	vector[i] = vector[j];
+	vector[j] = hold;
+}
+
+int standardPartition (int* vector, int startIndex, int endIndex)
+{
+	int x;
+	int i, j;
+
+	x = vector[endIndex];
+	i = startIndex - 1;
+
+	for (j = startIndex; j < endIndex; j++)
+	{
+		if (vector[j] <= x)
+		{
+			i++;
+			troca (vector, i, j);
+		}
+	}
+
+	troca (vector, i + 1, endIndex);
+	return i + 1;
+}
+
+void standardQuickSort (int* vector, int left, int right)
+{
+	int q;
+
+	if (left < right)
+	{
+		q = standardPartition (vector, left, right);
+		standardQuickSort(vector, left, q - 1);
+		standardQuickSort(vector, q + 1, right);
+	}
+}
