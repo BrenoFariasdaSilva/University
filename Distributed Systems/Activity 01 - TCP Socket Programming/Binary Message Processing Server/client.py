d1d1eb122fd2570fe736192b20c5c5d24553aa40
BrenoFariasdaSilva <breno.vicio78@gmail.com>
2023-04-23 13:31:36 -0300

    SD Activity - refactoring client

--- Distributed Systems/Activity 01 - TCP Socket Programming/Binary Message Processing Server/client.py
+++ Distributed Systems/Activity 01 - TCP Socket Programming/Binary Message Processing Server/client.py
@@ -18,11 +18,12 @@ ADDFILE = 1 # ADDFILE command
 DELETE = 2 # DELETE command
 GETFILESLIST = 3 # GETFILESLIST command
 GETFILE = 4 # GETFILE command
+EXIT = 5 # EXIT command
 standardResponseHeaderSize = 3 # Size of the standard response header
 statusCodePosition = 2 # Position of the status code in the response header
 
 # Valid commands for the client:
-validCommands = ["ADDFILE", "DELETE", "GETFILESLIST", "GETFILE"]
+validCommands = ["ADDFILE", "DELETE", "GETFILESLIST", "GETFILE", "EXIT"]
 
 # Header of the request message:
 standardRequestHeader = bytearray(3) # 1 Byte for the message type, 1 Byte for the command and 1 Byte for the size of the filename
@@ -51,85 +52,95 @@ def switch(command, filename):
     if not os.path.exists("./client"): # If the directory doesn't exist
         os.makedirs("./client") # Create the directory
         
-    if (command == "EXIT"):
-        # send the exit command to the server
-        standardRequestHeader[1] = 5 # 5 is the exit command
-        clientSocket.send(standardRequestHeader)
-        print(backgroundColors.OKGREEN + "Connection closed" + Style.RESET_ALL)
-        clientSocket.close()
-        exit()
-            
-    if command == validCommands[0]: # ADDFILE
-        standardRequestHeader[1] = ADDFILE # ADDFILE in the header inside position 1
-        standardRequestHeader[2] = len(filename) # Size of the filename in the header inside position 2
-        directoryFiles = os.listdir("./client") # List of files in the directory
-
-        if (directoryFiles.__contains__(filename)):
-            if (len(filename) < MAXFILENAMESIZE):
-                clientSocket.send(standardRequestHeader + bytearray(filename.encode())) # Send the standard header and the filename
-                fileSize = os.path.getsize("./client/" + filename) # Size of the file in bytes and in Big Endian
-                clientSocket.send(fileSize.to_bytes(4, "big")) # Send the size of the file in bytes with a size of 4 bytes  
-                
-                OpenFile = open("./client/" + filename, "rb") # Open the file in binary mode
-                fileBytes = OpenFile.read() # Read the file in bytes
-                clientSocket.send(fileBytes) # Send the file in bytes to the server
-
-                confirmation = int(clientSocket.recv(standardResponseHeaderSize)[statusCodePosition]) # Confirmation if the file was added or not
-                
-                if (confirmation == SUCESS): # If the file was added
-                    print(backgroundColors.OKGREEN + "File " + backgroundColors.OKCYAN + filename + backgroundColors.OKGREEN + " transfered!" + Style.RESET_ALL)
-                else: # If the file was not added
-                    print(backgroundColors.FAIL + "ERROR transfering the file " + backgroundColors.OKCYAN + filename + "!" + Style.RESET_ALL)
-
-        else:
-            print(backgroundColors.FAIL + "File " + backgroundColors.OKCYAN + filename + backgroundColors.FAIL + " not found!" + Style.RESET_ALL)
-            
+    if (command == validCommands[4]): # EXIT
+        client_exit()
+    elif command == validCommands[0]: # ADDFILE
+        addfile(filename)
     elif command == validCommands[1]: # DELETE
-        standardRequestHeader[1] = DELETE # DELETE in the header inside position 1
-        standardRequestHeader[2] = len(filename) # Size of the filename in the header inside position 2
-        clientSocket.send(standardRequestHeader + bytearray(filename.encode())) # Send the standard header and the filename
-        
-        confirmation = clientSocket.recv(standardResponseHeaderSize)[statusCodePosition]
-
-        if (confirmation == SUCESS): # If the file was deleted
-            print(backgroundColors.OKGREEN + "File " + backgroundColors.OKCYAN + filename + backgroundColors.OKGREEN + " deleted!" + Style.RESET_ALL)
-        else: # If the file was not deleted
-            print(backgroundColors.FAIL + "ERROR deleting the file " + backgroundColors.OKCYAN + filename + "!" + Style.RESET_ALL)
-            
+        delete(filename)
     elif command == validCommands[2]: # GETFILESLIST
-        standardRequestHeader[1] = GETFILESLIST # GETFILESLIST in the header inside position 1
-        standardRequestHeader[2] = 0 # Size of the filename in the header inside position 2
-        clientSocket.send(standardRequestHeader) # Send the standard header
-        
-        confirmation = clientSocket.recv(standardResponseHeaderSize)[statusCodePosition]
-
-        if (confirmation == SUCESS):
-            quantityFiles = int.from_bytes(clientSocket.recv(2), "big") # Quantity of files in the server in Big Endian
-            print(f"{backgroundColors.OKGREEN}Files in the server: {backgroundColors.OKCYAN}{quantityFiles}{Style.RESET_ALL}")
-            for i in range(quantityFiles): # Print the files in the server
-                filenameSize = int.from_bytes(clientSocket.recv(1), "big") # Size of the file in bytes in Big Endian
-                print(backgroundColors.OKCYAN + clientSocket.recv(filenameSize).decode() + Style.RESET_ALL) # Print the file name
-        else:
-            print(backgroundColors.FAIL + "ERROR getting the files list: Empty Directory!" + Style.RESET_ALL)
-            
+        getfileslist()
     elif command == validCommands[3]: # GETFILE
-        standardRequestHeader[1] = GETFILE # GETFILE in the header inside position 1
-        standardRequestHeader[2] = len(filename) # Size of the filename in the header inside position 2
-        clientSocket.send(standardRequestHeader + bytearray(filename.encode())) # Send the standard header and the filename
-
-        if (clientSocket.recv(standardResponseHeaderSize)[statusCodePosition] == SUCESS): # If the file was found
-            fileSize = int.from_bytes(clientSocket.recv(4), byteorder="big") # Size of the file in bytes in Big Endian
-            file = b'' # File in bytes
-            file = clientSocket.recv(fileSize) # Recieve the file in bytes
-
-            with open("./client/" + filename, "w+b") as files: # Create a file with the name of the file in the server
-                files.write(file) # Write the file in bytes in the file created
-            print(backgroundColors.OKGREEN + "File " + backgroundColors.OKCYAN + filename + backgroundColors.OKGREEN + " transfered!" + Style.RESET_ALL)
-        else:
-            print(backgroundColors.FAIL + "ERROR recieving the file " + backgroundColors.OKCYAN + filename + "!" + Style.RESET_ALL)
-            
+        getfile(filename)
     else:
         print(backgroundColors.FAIL + "Invalid command!" + Style.RESET_ALL)
+        
+def client_exit():
+    standardRequestHeader[1] = EXIT # EXIT in the header inside position 1
+    standardRequestHeader[2] = 0 # Size of the filename in the header inside position 2
+    clientSocket.send(standardRequestHeader) # Send the standard header
+    clientSocket.close() # Close the socket
+    print(backgroundColors.OKGREEN + "Connection closed!" + Style.RESET_ALL)
+    exit(0)
+    
+def addfile(filename):
+    standardRequestHeader[1] = ADDFILE # ADDFILE in the header inside position 1
+    standardRequestHeader[2] = len(filename) # Size of the filename in the header inside position 2
+    directoryFiles = os.listdir("./client") # List of files in the directory
+
+    if (directoryFiles.__contains__(filename)):
+        if (len(filename) < MAXFILENAMESIZE):
+            clientSocket.send(standardRequestHeader + bytearray(filename.encode())) # Send the standard header and the filename
+            fileSize = os.path.getsize("./client/" + filename) # Size of the file in bytes and in Big Endian
+            clientSocket.send(fileSize.to_bytes(4, "big")) # Send the size of the file in bytes with a size of 4 bytes  
+            
+            OpenFile = open("./client/" + filename, "rb") # Open the file in binary mode
+            fileBytes = OpenFile.read() # Read the file in bytes
+            clientSocket.send(fileBytes) # Send the file in bytes to the server
+
+            confirmation = int(clientSocket.recv(standardResponseHeaderSize)[statusCodePosition]) # Confirmation if the file was added or not
+            
+            if (confirmation == SUCESS): # If the file was added
+                print(backgroundColors.OKGREEN + "File " + backgroundColors.OKCYAN + filename + backgroundColors.OKGREEN + " transfered!" + Style.RESET_ALL)
+            else: # If the file was not added
+                print(backgroundColors.FAIL + "ERROR transfering the file " + backgroundColors.OKCYAN + filename + "!" + Style.RESET_ALL)
+
+    else:
+        print(backgroundColors.FAIL + "File " + backgroundColors.OKCYAN + filename + backgroundColors.FAIL + " not found!" + Style.RESET_ALL)
+    
+def delete(filename):
+    standardRequestHeader[1] = DELETE # DELETE in the header inside position 1
+    standardRequestHeader[2] = len(filename) # Size of the filename in the header inside position 2
+    clientSocket.send(standardRequestHeader + bytearray(filename.encode())) # Send the standard header and the filename
+    
+    confirmation = clientSocket.recv(standardResponseHeaderSize)[statusCodePosition]
+
+    if (confirmation == SUCESS): # If the file was deleted
+        print(backgroundColors.OKGREEN + "File " + backgroundColors.OKCYAN + filename + backgroundColors.OKGREEN + " deleted!" + Style.RESET_ALL)
+    else: # If the file was not deleted
+        print(backgroundColors.FAIL + "ERROR deleting the file " + backgroundColors.OKCYAN + filename + "!" + Style.RESET_ALL)
+        
+def getfileslist():
+    standardRequestHeader[1] = GETFILESLIST # GETFILESLIST in the header inside position 1
+    standardRequestHeader[2] = 0 # Size of the filename in the header inside position 2
+    clientSocket.send(standardRequestHeader) # Send the standard header
+    
+    confirmation = clientSocket.recv(standardResponseHeaderSize)[statusCodePosition]
+
+    if (confirmation == SUCESS):
+        quantityFiles = int.from_bytes(clientSocket.recv(2), "big") # Quantity of files in the server in Big Endian
+        print(f"{backgroundColors.OKGREEN}Files in the server: {backgroundColors.OKCYAN}{quantityFiles}{Style.RESET_ALL}")
+        for i in range(quantityFiles): # Print the files in the server
+            filenameSize = int.from_bytes(clientSocket.recv(1), "big") # Size of the file in bytes in Big Endian
+            print(backgroundColors.OKCYAN + clientSocket.recv(filenameSize).decode() + Style.RESET_ALL) # Print the file name
+    else:
+        print(backgroundColors.FAIL + "ERROR getting the files list: Empty Directory!" + Style.RESET_ALL)
+    
+def getfile(filename):
+    standardRequestHeader[1] = GETFILE # GETFILE in the header inside position 1
+    standardRequestHeader[2] = len(filename) # Size of the filename in the header inside position 2
+    clientSocket.send(standardRequestHeader + bytearray(filename.encode())) # Send the standard header and the filename
+
+    if (clientSocket.recv(standardResponseHeaderSize)[statusCodePosition] == SUCESS): # If the file was found
+        fileSize = int.from_bytes(clientSocket.recv(4), byteorder="big") # Size of the file in bytes in Big Endian
+        file = b'' # File in bytes
+        file = clientSocket.recv(fileSize) # Recieve the file in bytes
+
+        with open("./client/" + filename, "w+b") as files: # Create a file with the name of the file in the server
+            files.write(file) # Write the file in bytes in the file created
+        print(backgroundColors.OKGREEN + "File " + backgroundColors.OKCYAN + filename + backgroundColors.OKGREEN + " transfered!" + Style.RESET_ALL)
+    else:
+        print(backgroundColors.FAIL + "ERROR recieving the file " + backgroundColors.OKCYAN + filename + "!" + Style.RESET_ALL)
 
 if __name__ == "__main__":
     main()
