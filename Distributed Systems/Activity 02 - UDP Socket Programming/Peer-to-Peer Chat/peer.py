1c02568ee1666ad70122731fabbf0a2bf9c10ae5
Breno Farias da Silva <breno.vicio78@gmail.com>
2023-04-25 22:55:38 -0300

    Continuing the client side of the p2p

--- Distributed Systems/Activity 02 - UDP Socket Programming/Peer-to-Peer Chat/peer.py
+++ Distributed Systems/Activity 02 - UDP Socket Programming/Peer-to-Peer Chat/peer.py
@@ -3,6 +3,7 @@
 import socket
 import threading
 import emoji
+import emojis
 
 from colorama import Style
 
@@ -15,7 +16,7 @@ class backgroundColors:
     FAIL = "\033[91m"
 ## Host and port
 HOST = "localhost"
-PORTS = [5000, 6666]
+PORTS = [6666, 7000]
 
 DATAGRAM_SIZE = 1 + 1 + 64 + 1 + 255
 NORMALMESSAGE = 1
@@ -35,7 +36,7 @@ def main():
         username = input()
         if (validateUsername(username)):
             print(f"{backgroundColors.OKGREEN}Welcome {username}!{Style.RESET_ALL}")
-            createThreads()
+            createThreads(username)
             break
         else:
             print(f"{backgroundColors.FAIL}Invalid username. Please try again.{Style.RESET_ALL}")
@@ -45,16 +46,16 @@ def validateUsername(username):
         return True
     return False
     
-def createThreads():
+def createThreads(username):
     option = "undefined"
     while option != "server" and option != "client":
         print(f"{backgroundColors.OKGREEN}Will you be acting as a server or a client?{Style.RESET_ALL}")
-        option = input().toLowerCase()
+        option = input().lower()
         if (option == "server"):
             threading.Thread(target=server, args=(HOST, PORTS[0])).start() 
-            threading.Thread(target=client, args=(HOST, PORTS[1])).start() # TODO: Create client function
+            threading.Thread(target=client, args=(HOST, PORTS[1], username)).start()
         elif (option == "client"):
-            threading.Thread(target=client, args=(HOST, PORTS[0])).start()
+            threading.Thread(target=client, args=(HOST, PORTS[0], username)).start()
             threading.Thread(target=server, args=(HOST, PORTS[1])).start()
         else:
             print(f"{backgroundColors.FAIL}Invalid option. Please try again.{Style.RESET_ALL}")
@@ -69,9 +70,9 @@ def server(HOST, PORT):
         nick = data[2:nickSize+2].decode()
         messageSize = int(data[nickSize+2])
         messageContent = data[nickSize+3 : nickSize+3+messageSize].decode() 
-        messageFormat(nick, messageType, messageContent)
+        messageFormat(nick, messageType, messageContent, PORT)
 
-def messageFormat(nick, messageType, messageContent):
+def messageFormat(nick, messageType, messageContent, PORT):
     if messageType == NORMALMESSAGE:
         print(f"{backgroundColors.OKGREEN}{nick}{backgroundColors.OKCYAN}: {messageContent}{Style.RESET_ALL}")
     elif messageType == EMOJI:
@@ -79,7 +80,7 @@ def messageFormat(nick, messageType, messageContent):
     elif messageType == URL:
         print(f"{backgroundColors.OKGREEN}{nick}{backgroundColors.OKCYAN}: {messageContent}{Style.RESET_ALL}")
     elif messageType == ECHO:
-        clientSocket.sendto(createDatagram(nick, NORMALMESSAGE, messageContent), (HOST, PORTS[1]))
+        clientSocket.sendto(createDatagram(nick, NORMALMESSAGE, messageContent), (HOST, PORT))
     else:
         print(f"{backgroundColors.FAIL}Invalid message type.{Style.RESET_ALL}")
 
@@ -89,7 +90,7 @@ def createDatagram(nick, messageType, messageContent):
     datagram = bytes([messageType, nickSize]) + nick.encode() + bytes([messageSize]) + messageContent.encode()
     return datagram
 
-def client(HOST, PORT):
+def client(HOST, PORT, username):
     while True:
         print(f"{backgroundColors.OKGREEN}Please enter your message:{Style.RESET_ALL}")
         message = input()
@@ -97,9 +98,17 @@ def client(HOST, PORT):
             break
         elif (len(message) < 0 and len(message) > 256):
             print(f"{backgroundColors.FAIL}Invalid message. Please try again.{Style.RESET_ALL}")
-        # else:
+        else:
             # TODO: Validate message type
             # TODO: Send message to server
+            messageType = NORMALMESSAGE
+            if (emojis.count(message) > 0):
+                messageType = EMOJI
+            elif (message.__contains__("http://") or message.__contains__("https://")):
+                messageType = URL
+            elif (message.__contains__("echo")):
+                messageType = ECHO
+            clientSocket.sendto(createDatagram(username, messageType, message), (HOST, PORT))
             
 # This function is the one who calls the main functions of the program
 if __name__ == "__main__":
