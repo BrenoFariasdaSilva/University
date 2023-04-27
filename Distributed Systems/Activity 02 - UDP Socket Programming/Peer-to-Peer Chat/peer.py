eed14cd441cc0352d9707338e79b7089749cf100
BrenoFariasdaSilva <breno.vicio78@gmail.com>
2023-04-27 16:38:44 -0300

    Fixing ECHO logic

--- Distributed Systems/Activity 02 - UDP Socket Programming/Peer-to-Peer Chat/peer.py
+++ Distributed Systems/Activity 02 - UDP Socket Programming/Peer-to-Peer Chat/peer.py
@@ -3,8 +3,9 @@
 import socket
 import threading
 import emoji
-import emojis
+import re
 
+from emoji.core import emoji_count
 from colorama import Style
 
 # Macros:
@@ -14,15 +15,22 @@ class backgroundColors:
     OKGREEN = "\033[92m"
     WARNING = "\033[93m"
     FAIL = "\033[91m"
+
 ## Host and port
 HOST = "localhost"
 PORTS = [6666, 7000]
 
+# Datagram size
 DATAGRAM_SIZE = 1 + 1 + 64 + 1 + 255
+
+# Message types
 NORMALMESSAGE = 1
 EMOJI = 2
 URL = 3
 ECHO = 4
+
+# Regex for the URLs
+URL_REGEX = r"(http|https)://[a-zA-Z0-9\./]+"
 
 # Create a socket object
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP socket
@@ -60,29 +68,30 @@ def createThreads(username):
         else:
             print(f"{backgroundColors.FAIL}Invalid option. Please try again.{Style.RESET_ALL}")
 
-def server(HOST, PORT):
+def server(HOST, PORT): # Receive messages
     clientSocket.bind((HOST, PORT))
     while True:
-        data, addr = clientSocket.recvfrom(DATAGRAM_SIZE)
+        datagram, peerAddress = clientSocket.recvfrom(DATAGRAM_SIZE)
         print('')
-        messageType = int(data[0])
-        nickSize = int(data[1])
-        nick = data[2:nickSize+2].decode()
-        messageSize = int(data[nickSize+2])
-        messageContent = data[nickSize+3 : nickSize+3+messageSize].decode() 
-        messageFormat(nick, messageType, messageContent, PORT)
+        messageType = int(datagram[0])
+        nickSize = int(datagram[1])
+        nick = datagram[2:nickSize+2].decode()
+        messageSize = int(datagram[nickSize+2])
+        messageContent = datagram[nickSize+3 : nickSize+3+messageSize].decode() 
+        messageFormat(nick, messageType, messageContent, peerAddress[1])
 
 def messageFormat(nick, messageType, messageContent, PORT):
     if messageType == NORMALMESSAGE:
-        print(f"{backgroundColors.OKGREEN}{nick}{backgroundColors.OKCYAN}: {messageContent}{Style.RESET_ALL}")
+        print(f"{backgroundColors.OKGREEN}{nick} Normal Message{backgroundColors.OKCYAN}: {messageContent}{Style.RESET_ALL}")
     elif messageType == EMOJI:
-        print(f"{backgroundColors.OKGREEN}{nick}{backgroundColors.OKCYAN}: {emoji.emojize(messageContent)}{Style.RESET_ALL}")
+        print(f"{backgroundColors.OKGREEN}{nick} EMOJI{backgroundColors.OKCYAN}: {emoji.emojize(messageContent)}{Style.RESET_ALL}")
     elif messageType == URL:
-        print(f"{backgroundColors.OKGREEN}{nick}{backgroundColors.OKCYAN}: {messageContent}{Style.RESET_ALL}")
+        print(f"{backgroundColors.OKGREEN}{nick} URL{backgroundColors.OKCYAN}: {messageContent}{Style.RESET_ALL}")
     elif messageType == ECHO:
+        print(f"{backgroundColors.OKGREEN}{nick} ECHO{backgroundColors.OKCYAN}: {messageContent}{Style.RESET_ALL}")
         clientSocket.sendto(createDatagram(nick, NORMALMESSAGE, messageContent), (HOST, PORT))
     else:
-        print(f"{backgroundColors.FAIL}Invalid message type.{Style.RESET_ALL}")
+        print(f"{backgroundColors.FAIL}Invalid message type.{Style.RESET_ALL}", end=" ")
 
 def createDatagram(nick, messageType, messageContent):
     nickSize = len(nick)
@@ -90,26 +99,32 @@ def createDatagram(nick, messageType, messageContent):
     datagram = bytes([messageType, nickSize]) + nick.encode() + bytes([messageSize]) + messageContent.encode()
     return datagram
 
-def client(HOST, PORT, username):
+def isEcho(message):
+    command = message.split(" ")[0]
+    if (command.upper() == "ECHO"):
+        message = message[5:]
+        return True
+    return False
+
+def client(HOST, PORT, username): # Send messages
     while True:
-        print(f"{backgroundColors.OKGREEN}Please enter your message:{Style.RESET_ALL}")
+        print(f"{backgroundColors.OKGREEN}Please enter your message:{Style.RESET_ALL}", end=" ")
         message = input()
-        if (message == "exit"):
+
+        if (message.lower() == "exit"):
             break
         elif (len(message) < 0 and len(message) > 256):
             print(f"{backgroundColors.FAIL}Invalid message. Please try again.{Style.RESET_ALL}")
         else:
-            # TODO: Validate message type
-            # TODO: Send message to server
             messageType = NORMALMESSAGE
-            if (emojis.count(message) > 0):
+            if (emoji_count(emoji.emojize(message)) > 0):
                 messageType = EMOJI
-            elif (message.__contains__("http://") or message.__contains__("https://")):
+            elif (re.search(URL_REGEX, message)):
                 messageType = URL
-            elif (message.__contains__("echo")):
+            elif (isEcho(message)):
                 messageType = ECHO
             clientSocket.sendto(createDatagram(username, messageType, message), (HOST, PORT))
-            
+      
 # This function is the one who calls the main functions of the program
 if __name__ == "__main__":
     main()
