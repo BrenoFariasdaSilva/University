1c4e66d730eea91d97c0b8ab8abd485af7de20ad
BrenoFariasdaSilva <breno.vicio78@gmail.com>
2023-04-27 17:01:26 -0300

    Adding proper code documentation

--- Distributed Systems/Activity 02 - UDP Socket Programming/Peer-to-Peer Chat/peer.py
+++ Distributed Systems/Activity 02 - UDP Socket Programming/Peer-to-Peer Chat/peer.py
@@ -1,42 +1,50 @@
 # This file handles the peer operations
+# TODO: Validate the size of each field of the datagram -> nick, message, etc.
 
-import socket
-import threading
-import emoji
-import re
+import socket # Library for using sockets
+import threading # Library for using threads 
+import emoji # Library for using emojis
+import re # Library for using regular expressions
 
-from emoji.core import emoji_count
-from colorama import Style
+from emoji.core import emoji_count # Library for using emojis
+from colorama import Style # Library for using colors in the terminal
 
 # Macros:
 ## Colors for the terminal
 class backgroundColors: 
-    OKCYAN = "\033[96m"
-    OKGREEN = "\033[92m"
-    WARNING = "\033[93m"
-    FAIL = "\033[91m"
+    OKCYAN = "\033[96m" # Cyan color
+    OKGREEN = "\033[92m" # Green color
+    WARNING = "\033[93m" # Yellow color
+    FAIL = "\033[91m" # Red color
 
 ## Host and port
-HOST = "localhost"
-PORTS = [6666, 7000]
+HOST = "localhost" # Localhost
+PORTS = [6666, 7000] # Ports, first for "server", second for "client"
 
 # Datagram size
 DATAGRAM_SIZE = 1 + 1 + 64 + 1 + 255
+# 1 byte for the message type,
+# 1 byte for the nickname size,
+# 64 bytes for the nickname,
+# 1 byte for the message size,
+# 255 bytes for the message
 
 # Message types
-NORMALMESSAGE = 1
-EMOJI = 2
-URL = 3
-ECHO = 4
+NORMALMESSAGE = 1 # Normal message
+EMOJI = 2 # Emoji
+URL = 3 # URL
+ECHO = 4 # Echo
 
 # Regex for the URLs
-URL_REGEX = r"(http|https)://[a-zA-Z0-9\./]+"
+URL_REGEX = r"(http|https)://[a-zA-Z0-9\./]+" 
 
 # Create a socket object
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP socket
 
 # This is the main function
-# It receives the user input
+# @param: None
+# @return: None
+# @logic: This function will read user input, validate it and call the functions to manage the threads
 def main():
     print(f"{backgroundColors.OKCYAN}Welcome to the P2P chat!{Style.RESET_ALL}")
     while True:
@@ -49,11 +57,20 @@ def main():
         else:
             print(f"{backgroundColors.FAIL}Invalid username. Please try again.{Style.RESET_ALL}")
 
+
+# This function validates the username
+# @param username: The username to validate
+# @return: True if the username is valid, False otherwise
+# @logic: This function will validate the username according to the length valid range for the datagram
 def validateUsername(username):
     if (len(username) > 0 and len(username) < 65):
         return True
     return False
-    
+
+# This function creates the threads 
+# @param username: The username of the peer
+# @return: None
+# @logic: This function will create the threads for the server and the client according to the user input
 def createThreads(username):
     option = "undefined"
     while option != "server" and option != "client":
@@ -68,6 +85,11 @@ def createThreads(username):
         else:
             print(f"{backgroundColors.FAIL}Invalid option. Please try again.{Style.RESET_ALL}")
 
+# This function represents the server
+# @param HOST: The host of the peer
+# @param PORT: The port of the peer
+# @return: None
+# @logic: This function will receive the messages and call the function to format them
 def server(HOST, PORT): # Receive messages
     clientSocket.bind((HOST, PORT))
     while True:
@@ -80,6 +102,13 @@ def server(HOST, PORT): # Receive messages
         messageContent = datagram[nickSize+3 : nickSize+3+messageSize].decode() 
         messageFormat(nick, messageType, messageContent, peerAddress[1])
 
+# This function formats the messages
+# @param nick: The nickname of the peer
+# @param messageType: The type of the message
+# @param messageContent: The content of the message
+# @param PORT: The port of the peer
+# @return: None
+# @logic: This function will format the messages according to the type of the message
 def messageFormat(nick, messageType, messageContent, PORT):
     if messageType == NORMALMESSAGE:
         print(f"{backgroundColors.OKGREEN}{nick} Normal Message{backgroundColors.OKCYAN}: {messageContent}{Style.RESET_ALL}")
@@ -93,12 +122,22 @@ def messageFormat(nick, messageType, messageContent, PORT):
     else:
         print(f"{backgroundColors.FAIL}Invalid message type.{Style.RESET_ALL}", end=" ")
 
+# This function creates the datagram
+# @param nick: The nickname of the peer
+# @param messageType: The type of the message
+# @param messageContent: The content of the message
+# @return: The datagram
+# @logic: This function will create the datagram according to protocol specifications
 def createDatagram(nick, messageType, messageContent):
     nickSize = len(nick)
     messageSize = len(messageContent)
     datagram = bytes([messageType, nickSize]) + nick.encode() + bytes([messageSize]) + messageContent.encode()
     return datagram
 
+# This function verify if the message is an echo
+# @param message: The message to verify
+# @return: True if the message is an echo, False otherwise
+# @logic: This function will verify if the message is an echo by splitting the message and checking if the first word is "ECHO"
 def isEcho(message):
     command = message.split(" ")[0]
     if (command.upper() == "ECHO"):
@@ -106,6 +145,12 @@ def isEcho(message):
         return True
     return False
 
+# This function handles the client
+# @param HOST: The host of the peer
+# @param PORT: The port of the peer
+# @param username: The username of the peer
+# @return: None
+# @logic: This function will send the messages to the server according to the user input
 def client(HOST, PORT, username): # Send messages
     while True:
         print(f"{backgroundColors.OKGREEN}Please enter your message:{Style.RESET_ALL}", end=" ")
@@ -126,5 +171,8 @@ def client(HOST, PORT, username): # Send messages
             clientSocket.sendto(createDatagram(username, messageType, message), (HOST, PORT))
       
 # This function is the one who calls the main functions of the program
+# @param: None
+# @return: None
+# @logic: This function will call the main functions of the program
 if __name__ == "__main__":
     main()
