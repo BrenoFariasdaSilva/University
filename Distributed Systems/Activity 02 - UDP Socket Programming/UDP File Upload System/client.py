76267620199a640e01b5294d46c0dc54e227a32d
BrenoFariasdaSilva <breno.vicio78@gmail.com>
2023-05-01 17:37:25 -0300

    Developing server.py

--- Distributed Systems/Activity 02 - UDP Socket Programming/UDP File Upload System/client.py
+++ Distributed Systems/Activity 02 - UDP Socket Programming/UDP File Upload System/client.py
@@ -3,6 +3,7 @@
 # 1. The first datagram will be the file size (4 bytes), filename size (4 bytes), filename (984 bytes), and the file hash (32 bytes)
 # 2. The rest of the datagrams will be the file data (1024 bytes)
 
+import math
 import socket # For creating the UDP/Datagram socket
 import threading # For creating the client thread
 import hashlib # For getting the file hash (SHA256)
@@ -16,12 +17,26 @@ class backgroundColors: # Colors for the terminal
 	FAIL = "\033[91m" # Red
 
 # Constants:
+SERVERADDRESS = ('localhost', 7000) # The server to send the file to
 DATAGRAMSIZE = 1024 # The size of the data chunk to send
 
 # Commands
 EXIT = "exit" # Exit command
 HELP = "help" # Help command
 UPLOAD = "upload" # Upload command
+
+# @brief: This function waits for the server to send a datagram, related to "OK" or "ERROR" file upload
+# @param client_socket: The socket to receive the datagram from
+# @param filename: The name of the file to re-upload if the file upload failed
+# @return: The datagram received from the server
+def waitForServerResponse(client_socket, filename):
+	datagram, server = client_socket.recvfrom(DATAGRAMSIZE) # Wait for the server to send a datagram
+	
+	if datagram.decode() == "OK":
+		print(f"{backgroundColors.OKGREEN}File uploaded successfully!{Style.RESET_ALL}")
+	elif datagram.decode() == "ERROR":
+		print(f"{backgroundColors.FAIL}File upload failed{Style.RESET_ALL}")
+		upload_file(filename, client_socket) # Re-upload the file
 
 # @brief: Verifies that the file exists
 # @param filename: The name of the file to verify
@@ -37,10 +52,9 @@ def verify_filename(filename):
 # @brief: Processes the file and send the first datagram, then send the rest of the datagrams in a loop until the file is sent.
 # @param filename: The name of the file to upload
 # @param client_socket: The socket to send the file to the server
-# @param server: The server to send the file to
 # @return: None
-def upload_file(filename, client_socket, server):
-	file = open(filename, 'rb') # Open the file in binary mode
+def upload_file(filename, client_socket):
+	file = open("./client_files/" + filename, 'rb') # Open the file in binary mode
 	file_data = file.read() # Read the file data
 	file.close() # Close the file
 
@@ -50,45 +64,50 @@ def upload_file(filename, client_socket, server):
 	filename_size = len(filename) # Get the filename size
 
 	# Convert the file size, filename size, filename, and file hash to bytes
-	first_datagram = file_size.to_bytes(4, 'big') + filename_size.to_bytes(4, 'big') + filename.encode() + file_hash.encode() 
-	client_socket.sendto(first_datagram, server) # Send the first datagram
+	first_datagram = file_size.to_bytes(4, 'big') + filename_size.to_bytes(4, 'big') + bytes(filename, 'utf-8') + bytes(file_hash, 'utf-8')
+	client_socket.sendto(first_datagram, SERVERADDRESS) # Send the first datagram
 
-	# Send the rest of the datagrams
-	for i in range(0, file_size, DATAGRAMSIZE): # Loop through the file data
-		client_socket.sendto(file_data[i:i+DATAGRAMSIZE], server) # Send the file data in 1024 byte chunks
+	# Send the filedata datagrams
+	for i in range(math.ceil(file_size / DATAGRAMSIZE)): # Loop through the file data
+		client_socket.sendto(file_data[i * DATAGRAMSIZE : (i + 1) * DATAGRAMSIZE], SERVERADDRESS) # Send the file data
+
+	waitForServerResponse(client_socket, filename) # Wait for the server to send a datagram
 
 # @brief: Uploads the file to the server
 # @param client_socket: The socket to send the file to the server
-# @param server: The server to send the file to
 # @return: None
-def clientThread(client_socket, server):
+def clientThread(client_socket):
 	while True:
-		print(f"{backgroundColors.OKCYAN}Type your command here: {Style.RESET_ALL}", end=" ")
+		print(f"{backgroundColors.OKGREEN}Type your command here: {Style.RESET_ALL}", end=" ")
 		userInput = input().split(" ") # Split the input by spaces
 
 		if userInput[0].lower() == EXIT: # Exit command
 			return
 		elif userInput[0].lower() == HELP: # Help command
-			print(f"{backgroundColors.OKCYAN}Valid commands: 1. {EXIT}\n2. {UPLOAD} filenameHere\n3. {HELP}{Style.RESET_ALL}")
+			printHelpCommands()
 		elif userInput[0].lower() == UPLOAD: # Upload command
 			if verify_filename(userInput[1]): # File exists
-				upload_file(userInput[1], client_socket, server) # Upload the file
+				upload_file(userInput[1], client_socket) # Upload the file
 				print(f"{backgroundColors.OKGREEN}File \"{userInput[1]}\" sent successfully!{Style.RESET_ALL}") 
 			else: # File does not exist
 				print(f"{backgroundColors.FAIL}File \"{userInput[1]}\" does not exist!{Style.RESET_ALL}")
 		else:
 			print(f"{backgroundColors.FAIL}Invalid command!{Style.RESET_ALL}")
 
+# @brief: Prints the help commands
+# @param: None
+# @return: None
+def printHelpCommands():
+	print(f"{backgroundColors.OKCYAN}Valid commands:\n1. {EXIT}\n2. {UPLOAD} filenameHere\n3. {HELP}{Style.RESET_ALL}")
+
 # @brief: Main function that runs the client program
 # @param: None
 # @return: None
 def main():
-	print(f"{backgroundColors.OKCYAN}Starting client...{Style.RESET_ALL}")
+	print(f"{backgroundColors.OKGREEN}Starting client...{Style.RESET_ALL}")
 	client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Create the UDP/Datagram socket
-	server = ('localhost', 7000) # The server to send the file to
-
-	print(f"{backgroundColors.OKCYAN}Valid commands: 1. {EXIT}\n2. {UPLOAD} filenameHere\n3. {HELP}{Style.RESET_ALL}")
-	client_thread = threading.Thread(target=clientThread, args=(client_socket, server)) # Create the client thread
+	printHelpCommands() # Print the help commands
+	client_thread = threading.Thread(target=clientThread, args=(client_socket,)) # Create the client thread
 	client_thread.start() # Start the client thread
 
 if __name__ == '__main__':
--- Distributed Systems/Activity 02 - UDP Socket Programming/UDP File Upload System/server.py
+++ Distributed Systems/Activity 02 - UDP Socket Programming/UDP File Upload System/server.py
@@ -1,18 +1,124 @@
-import socket
-import threading
-import hashlib
-from colorama import Style
+# @brief: This file contains the server program that will send files to the server
+# The received datagrams will be in the following format:
+# 1. The first datagram will be the file size (4 bytes), filename size (4 bytes), filename (984 bytes), and the file hash (32 bytes)
+# 2. The rest of the datagrams will be the file data (1024 bytes)
+
+import socket # For creating the UDP/Datagram socket
+import threading # For creating the server thread
+import hashlib # For getting the file hash (SHA256)
+from colorama import Style # For coloring the terminal
 
 # Macros:
 class backgroundColors: # Colors for the terminal
-	OKCYAN = "\033[96m"
-	OKGREEN = "\033[92m"
-	WARNING = "\033[93m"
-	FAIL = "\033[91m"
+	OKCYAN = "\033[96m" # Cyan
+	OKGREEN = "\033[92m" # Green
+	WARNING = "\033[93m" # Yellow
+	FAIL = "\033[91m" # Red
 
-def main():
-	print(f"{backgroundColors.OKCYAN}Starting server...{Style.RESET_ALL}")
+# Constants:
+DATAGRAMSIZE = 1024 # The size of the data chunk to send
+HOST = "localhost" # The server's IP address
+PORT = 7000 # The server's port
+
+# brief: Validate file integrity
+# @param filename: The name of the file to validate
+# @param file_hash: The file hash to validate
+# @return: True if the file is valid, False otherwise
+def validate_file(filename, file_hash):
+	file = open("./server_files/" + filename, 'rb') # Open the file in binary mode
+	file_data = file.read() # Read the file data
+	file.close() # Close the file
+
+	file_hash_calculated = hashlib.sha256(file_data).hexdigest() # Get the file hash
+
+	if file_hash == file_hash_calculated: # Check if the file hash is correct
+		return True
+	else:
+		return False
 	
+# @brief: This function wwrites the file data to the file
+# @param filename: The name of the file to write to
+# @param file_data: The file data to write to the file
+# @return: None
+def write_file(filename, file_data):
+	file = open("./server_files/" + filename, 'wb') # Open the file in binary mode
+	file.write(file_data) # Write the file data to the file
+	file.close() # Close the file
+
+# @brief: This function prints the file info from the first datagram
+# @param file_size: The file size
+# @param filename_size: The filename size
+# @param filename: The filename
+# @param file_hash: The file hash
+# @return: None
+def printFirstDatagram(file_size, filename_size, filename, file_hash):
+	print(f"{backgroundColors.OKGREEN}File size: {file_size} bytes")
+	print(f"Filename size: {filename_size} bytes")
+	print(f"Filename: {filename}")
+	print(f"File hash: {file_hash}{Style.RESET_ALL}")
+
+# @brief: This function returns the file size, filename size, filename, and file hash from the first datagram
+# @param datagram: The datagram to get the data from
+# @return: The file size, filename size, filename, and file hash
+def getFirstDatagramData(datagram):
+	file_size = int.from_bytes(datagram[0:4], 'big')
+	filename_size = int.from_bytes(datagram[4:8], 'big')
+	filename = datagram[8:8+filename_size].decode('utf-8')
+	file_hash = datagram[8+filename_size:8+filename_size+32].decode('utf-8')
+	return file_size, filename_size, filename, file_hash
+
+# @brief: This is the server thread that will handle the datagram
+# @param datagram: The datagram to handle
+# @param client: The client's address
+# @param server_socket: The server socket
+# @return: None
+def serverThread(datagram, client, server_socket):
+	# Get the file size, filename size, filename, and file hash from the first datagram
+	file_size, filename_size, filename, file_hash = getFirstDatagramData(datagram)
+
+	# printFirstDatagram(file_size, filename_size, filename, file_hash)
+
+	file_data = b'' # Initialize the file data
+	while True:
+		for i in range(0, file_size, DATAGRAMSIZE):
+			datagram, client = server_socket.recvfrom(DATAGRAMSIZE)
+			file_data += datagram
+		break
+	
+	print(f"{backgroundColors.OKGREEN}File data recieved{Style.RESET_ALL}")
+	write_file(filename, file_data) # Write the file data to the file
+
+	if validate_file(filename, file_hash): # Check if the file is valid, send OK
+		print(f"{backgroundColors.OKGREEN}File is valid{Style.RESET_ALL}")
+		server_socket.sendto(b"OK", client)
+	else:
+		print(f"{backgroundColors.FAIL}File is invalid{Style.RESET_ALL}")
+		server_socket.sendto(b"ERROR", client)
+		# TODO: Delete the file from the server
+
+# @brief: This the main function that will be run when the program is executed
+# This function creates the UDP/DGRAM socket and binds it to the host and port.
+# Then it waits for a datagram to be sent to it and creates a server thread to handle the datagram.
+# @param: None
+# @return: None
+def main():
+	print(f"{backgroundColors.OKGREEN}Starting server...{Style.RESET_ALL}")
+	server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Create the UDP/Datagram socket
+	server_socket.bind((HOST, PORT)) # Bind the socket to the host and port
+	# print that the user is waiting for a connection
+	print(f"{backgroundColors.OKGREEN}Waiting for connection...{Style.RESET_ALL}")
+
+	# while True:
+		# The server is waiting for a datagram to be sent to it.
+		# When it receives a datagram, it stores the datagram in the variable datagram and the client's address in the variable client.
+		# The client's address is a tuple containing the client's IP address and port.
+	[datagram, client] = server_socket.recvfrom(DATAGRAMSIZE)
+	
+	print(f"{backgroundColors.OKGREEN}Recieved connection from {client[0]}:{client[1]}{Style.RESET_ALL}")
+	serverThread(datagram, client, server_socket) # Create the server thread
+		# server_thread = threading.Thread(target=serverThread, args=(datagram, client, server_socket)) # Create the server thread
+		# server_thread.start() # Start the server thread
+
 
 if __name__ == '__main__':
 	main()
