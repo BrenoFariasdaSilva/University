816585b0652cbd67d3d3917eb892885086ea0765
BrenoFariasdaSilva <breno.vicio78@gmail.com>
2023-05-01 15:53:54 -0300

    Adding code documentation into client.py

--- Distributed Systems/Activity 02 - UDP Socket Programming/UDP File Upload System/README.md
+++ Distributed Systems/Activity 02 - UDP Socket Programming/UDP File Upload System/README.md
@@ -2,12 +2,12 @@
 # UDP Socket Programming
 @author Breno Farias da Silva    
 @date 29/04/2023  
-@last update 29/04/2021  
+@last update 01/05/2021  
 @[repository](https://github.com/BrenoFariasdaSilva/University/tree/main/Distributed%20Systems/Activity%2002%20-%20UDP%20Socket%20Programming/UDP%20File%20Upload%20System)  
 Programming a UDP socket which is used into a client-server connection to upload files from the client to the server. The server will receive the first datagram which contains the file size, filename and the file checksum. After that, the server receives fragments of the file into batches of 1024 bytes and then reassembles them into a single file using the fragment position as a reference. After that, the server will calculate the file checksum and then verify if it is equal to the checksum received from the client.
 **Initial datagram**:
-| File Size (4 bytes) | Filename (988 byte) | Checksum (32 bytes) |
-|:-:|---|---|
+| File Size (4 bytes) | Filename Size (4 bytes) | Filename (988 byte) | Checksum (32 bytes) |
+|:-:|---|---|---|
 
 **File fragments datagram**:
 | File Fragment (1024 bytes)|
--- Distributed Systems/Activity 02 - UDP Socket Programming/UDP File Upload System/client.py
+++ Distributed Systems/Activity 02 - UDP Socket Programming/UDP File Upload System/client.py
@@ -1,27 +1,92 @@
-import socket
-import threading
-import hashlib
-from colorama import Style
+# @brief: This file contains the client program that will send files to the server
+# The sent datagrams will be in the following format:
+# 1. The first datagram will be the file size (4 bytes), filename size (4 bytes), filename (984 bytes), and the file hash (32 bytes)
+# 2. The rest of the datagrams will be the file data (1024 bytes)
+
+import socket # For creating the UDP/Datagram socket
+import threading # For creating the client thread
+import hashlib # For getting the file hash (SHA256)
+from colorama import Style # For coloring the terminal
 
 # Macros:
 class backgroundColors: # Colors for the terminal
-	OKCYAN = "\033[96m"
-	OKGREEN = "\033[92m"
-	WARNING = "\033[93m"
-	FAIL = "\033[91m"
+	OKCYAN = "\033[96m" # Cyan
+	OKGREEN = "\033[92m" # Green
+	WARNING = "\033[93m" # Yellow
+	FAIL = "\033[91m" # Red
 
-def main():
-	client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	server = ('localhost', 7000)
+# Constants:
+DATAGRAMSIZE = 1024 # The size of the data chunk to send
 
+# Commands
+EXIT = "exit" # Exit command
+UPLOAD = "upload" # Upload command
+
+# @brief: Verifies that the file exists
+# @param filename: The name of the file to verify
+# @return: True if the file exists, False otherwise
+def verify_filename(filename):
+	try:
+		file = open(filename, 'rb')
+		file.close()
+		return True
+	except:
+		return False
+	
+# @brief: Processes the file and send the first datagram, then send the rest of the datagrams in a loop until the file is sent.
+# @param filename: The name of the file to upload
+# @param client_socket: The socket to send the file to the server
+# @param server: The server to send the file to
+# @return: None
+def upload_file(filename, client_socket, server):
+	file = open(filename, 'rb') # Open the file in binary mode
+	file_data = file.read() # Read the file data
+	file.close() # Close the file
+
+	file_hash = hashlib.sha256(file_data).hexdigest() # Get the file hash
+
+	file_size = len(file_data) # Get the file size
+	filename_size = len(filename) # Get the filename size
+
+	# Convert the file size, filename size, filename, and file hash to bytes
+	first_datagram = file_size.to_bytes(4, 'big') + filename_size.to_bytes(4, 'big') + filename.encode() + file_hash.encode() 
+	client_socket.sendto(first_datagram, server) # Send the first datagram
+
+	# Send the rest of the datagrams
+	for i in range(0, file_size, DATAGRAMSIZE): # Loop through the file data
+		client_socket.sendto(file_data[i:i+DATAGRAMSIZE], server) # Send the file data in 1024 byte chunks
+
+# @brief: Uploads the file to the server
+# @param client_socket: The socket to send the file to the server
+# @param server: The server to send the file to
+# @return: None
+def clientThread(client_socket, server):
 	while True:
-		print(f"{backgroundColors.OKCYAN}Enter message to send to server: {Style.RESET_ALL}", end=' ')
-		message = input('-> ')
-		client_socket.sendto(message.encode('utf-8'), server)
-		data, addr = client_socket.recvfrom(1024)
-		print(Style.RESET_ALL + backgroundColors.OKGREEN + 'Received from server: ' + data.decode('utf-8'))
+		print(f"{backgroundColors.OKCYAN}Type your command here: {Style.RESET_ALL}", end=" ")
+		userInput = input().split(" ") # Split the input by spaces
 
+		if userInput[0].lower() == EXIT: # Exit command
+			return
+		elif userInput[0].lower() == UPLOAD: # Upload command
+			if verify_filename(userInput[1]): # File exists
+				upload_file(userInput[1]) # Upload the file
+				print(f"{backgroundColors.OKGREEN}File \"{userInput[1]}\" sent successfully!{Style.RESET_ALL}") 
+			else: # File does not exist
+				print(f"{backgroundColors.FAIL}File \"{userInput[1]}\" does not exist!{Style.RESET_ALL}")
+		else:
+			print(f"{backgroundColors.FAIL}Invalid command!{Style.RESET_ALL}")
 
+# @brief: Main function that runs the client program
+# @param: None
+# @return: None
+def main():
+	print(f"{backgroundColors.OKCYAN}Starting client...{Style.RESET_ALL}")
+	client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Create the UDP/Datagram socket
+	server = ('localhost', 7000) # The server to send the file to
+
+	print(f"{backgroundColors.OKCYAN}Valid commands: 1. {EXIT}\n2. {UPLOAD} filenameHere{Style.RESET_ALL}")
+	client_thread = threading.Thread(target=clientThread, args=(client_socket, server)) # Create the client thread
+	client_thread.start() # Start the client thread
 
 if __name__ == '__main__':
 	main()
