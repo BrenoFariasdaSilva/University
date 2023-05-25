5fca84c8d9c7f019bf31bb965880aa49caa12be7
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-05-25 11:54:30 -0300

    Updating database documentation

--- Distributed Systems/Activity 03 - XDR External Data Representation/server/database/database.py
+++ Distributed Systems/Activity 03 - XDR External Data Representation/server/database/database.py
@@ -1,8 +1,8 @@
-import os
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-from dotenv import load_dotenv
-import json
+import os # For getting the environment variables
+from pymongo.mongo_client import MongoClient # For connecting to the database
+from pymongo.server_api import ServerApi # For connecting to the database
+from dotenv import load_dotenv # For getting the environment variables
+import json # For converting the movie object to JSON
 from colorama import Style # For coloring the terminal
 
 # Background colors:
@@ -13,10 +13,12 @@ class backgroundColors: # Colors for the terminal
 	FAIL = "\033[91m" # Red
 
 # Status messages:
-SUCCESS = 1
-FAILURE = 0
+SUCCESS = 1 # Success
+FAILURE = 0 # Failure
 
+# @brief: This class is used to connect to the database, as well as all the CRUD related methods.
 class MongoDatabase:
+	# Constructor:
 	def __init__(self):
 		load_dotenv()
 		uri = f"mongodb+srv://{os.getenv('USERNAME')}:{os.getenv('PASSWORD')}@{os.getenv('DATABASE_URL')}"
@@ -27,9 +29,15 @@ class MongoDatabase:
 		print(f"{backgroundColors.OKGREEN}Client Connected to {backgroundColors.OKCYAN}{database_name}{backgroundColors.OKGREEN} server using {backgroundColors.OKCYAN}{os.getenv('USERNAME')} {backgroundColors.OKGREEN}user{Style.RESET_ALL}")
 		print("")
 
+	## Methods:
+	# @brief: This method gets the connection information
+	# @return: A JSON object with the connection information
 	def connectionInfo(self):
 		return self.client.server_info()
 
+	# @brief: This method creates a movie in the database
+	# @param movie: A JSON object with the movie information
+	# @return: SUCCESS if the movie was created, None if not
 	def createMovie(self, movie):
 		movie = json.loads(movie)
 		if movie.get('id'):
@@ -40,28 +48,49 @@ class MongoDatabase:
 			return SUCCESS
 		return None
 	
+	# @brief: This method gets a movie in the database by its title
+	# @param movie_title: The title of the movie
+	# @return: The movie object if the movie was found, None if not
 	def getMovieByTitle(self, movie_title):
 		return self.collection.find_one({"title": movie_title})
 	
+	# @brief: This method updates a movie in the database
+	# @param old_movie_object: The old movie object
+	# @param new_movie_object: The new movie object
+	# @return: The new movie object if the movie was updated, None if not
 	def updateMovie(self, old_movie_object, new_movie_object):
-		self.delete_one({"title": old_movie_object["title"]})
-		return self.createMovie(new_movie_object)
+		if (self.deleteMovie(old_movie_object["title"])):
+			return self.createMovie(new_movie_object)
+		return None
 
+	# @brief: This method deletes a movie in the database
+	# @param movie_title: The title of the movie
+	# @return: The number of movies deleted
 	def deleteMovie(self, movie_title):
 		reponse_document = self.collection.delete_one({"title": movie_title})
 		if reponse_document.acknowledged: # If the movie was deleted
 			return reponse_document.deleted_count
 		return None
 	
+	# @brief: This method gets the movies in the database made by a specific actor/cast
+	# @param actor_name: The name of the actor
+	# @return: A list with all the movies in the database related to the actor
 	def listByActor(self, actor_name):
 		return list(self.collection.find({"cast": actor_name}))
-	
+
+	# @brief: This method gets the movies in the database made by a specific director
+	# @param genre: The genre of the movie
+	# @return: A list with all the movies in the database according to the genre
 	def listByGenre(self, genre):
 		return list(self.collection.find({"genres": genre}))
 	
+	# @brief: This method deletes all the movies in the database
+	# @param: None
+	# @return: The number of movies deleted
 	def eraseAllMovies(self):
 		response_document = self.collection.delete_many({})
 		return response_document.deleted_count
 	
+# Main:
 if __name__ == "__main__":
 	database = MongoDatabase()
