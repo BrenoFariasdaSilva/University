ffb869dc948ad69d6a8c8e01b44616955d0a1e5d
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-05-29 17:03:49 -0300

    Updating server and starting client

---
+++ Distributed Systems/Activity 04 - gRPC/client/gRPC/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import org.example.structs.*;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
