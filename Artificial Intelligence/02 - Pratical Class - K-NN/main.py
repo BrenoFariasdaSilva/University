fa8525f383ee47dc77b58417fae5cd7d90f8f4c4
Breno Farias <brenofarias@Brenos-MBP.local>
2023-09-29 18:41:52 -0300

    FEAT: Adding the Artificial Intelligence k-NN Pratical Class Files

---
+++ Artificial Intelligence/02 - Pratical Class - K-NN/main.py
@@ -0,0 +1,34 @@
+"""
+Example of how to use the k-NN classifier using the scikit-learn library.
+"""
+
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC
+
+data_sets = ["digits", "spam", "writer"]
+data_set = data_sets[0]
+
+for data_set in data_sets:
+	# Load the data
+	tr = np.loadtxt(f"./data/{data_set}/train.txt");
+	ts = np.loadtxt(f"./data/{data_set}/test.txt");
+	y_test = ts[:,-1]
+	y_train = tr[:,-1]
+	X_train = tr[:, 1 : -1]
+	X_test = ts[:, 1 : -1]
+			
+	# k-NN classifier
+	# Documentation: https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
+
+	neigh = KNeighborsClassifier(n_neighbors = 20, metric = "euclidean")
+	neigh.fit(X_train, y_train)
+	print(classification_report(y_test, neigh.predict(X_test)))
+
+	# class_counts = np.bincount(y_train.astype("int64"))
+
+	# # Print the class counts
+	# for class_label, count in enumerate(class_counts):
+	# 	if count > 0:
+	# 		print(f"Classe {class_label}: {count} amostra(s).")
