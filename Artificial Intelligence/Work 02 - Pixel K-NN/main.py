06495ddc14cf8984ce212fe250e31dc167e49826
Breno Farias <brenofarias@Brenos-MBP.local>
2023-10-02 20:46:58 -0300

    REFACTOR: Adding loops for the test of the K and training dataset percentage values for the digits classification K-NN project

--- Artificial Intelligence/Work 02 - Pixel K-NN/main.py
+++ Artificial Intelligence/Work 02 - Pixel K-NN/main.py
@@ -48,6 +48,19 @@ def main():
 	# Verify if the output directory exists, if not, it creates the output directory
 	verify_output_directory(OUTPUT_DIRECTORY)
 	
+	for neighbours_value in NEIGHBOURS_VALUES: # Loop through the neighbours values: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+		for x_split, y_split in SPLITS.items(): # Loop through the splits: 1x1, 2x2, 3x3 and 5x5.
+			for training_dataset_size in TRAINING_DATASET_SIZE: # Loop through the training dataset sizes
+				# The path for the training dataset file
+				training_file_path = f"{DATASETS_PATH['training']}/{x_split}x{y_split}-normalized-pixel_count{DATASET_FILES_FORMAT}"
+				# Load the training dataset
+				training_dataset = pd.read_csv(training_file_path)
+
+				# The path for the test dataset file
+				test_file_path = f"{DATASETS_PATH['test']}/{x_split}x{y_split}-normalized-pixel_count{DATASET_FILES_FORMAT}"
+
+				# Load the test dataset
+				test_dataset = pd.read_csv(test_file_path)
 
 # @brief: The entry point of the program
 # @param: None
