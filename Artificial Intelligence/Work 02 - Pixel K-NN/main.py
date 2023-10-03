4f84f9851481c51f59e9504fe2f94f0e233addf9
Breno Farias <brenofarias@Brenos-MBP.local>
2023-10-02 21:47:02 -0300

    REFACTOR: Updating the results dictionary values for the digits classification K-NN project

--- Artificial Intelligence/Work 02 - Pixel K-NN/main.py
+++ Artificial Intelligence/Work 02 - Pixel K-NN/main.py
@@ -3,8 +3,6 @@ import pandas as pd # Import the pandas module for data manipulation
 from colorama import Style # For coloring the terminal
 from tqdm import tqdm # For Generating the Progress Bars
 
-import time
-
 # Macros:
 class backgroundColors: # Colors for the terminal
 	CYAN = "\033[96m" # Cyan
@@ -67,50 +65,54 @@ def main():
 
 	results = initialize_results() # Initialize the results dictionary
 	
-	for neighbours_value in NEIGHBOURS_VALUES: # Loop through the neighbours values: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
-		for x_split, y_split in SPLITS.items(): # Loop through the splits: 1x1, 2x2, 3x3 and 5x5.
-			for training_dataset_size in TRAINING_DATASET_SIZE: # Loop through the training dataset sizes
-				# The path for the training dataset file
-				training_file_path = f"{DATASETS_PATH['training']}/{x_split}x{y_split}-normalized-pixel_count{DATASET_FILES_FORMAT}"
-				# Load the training dataset
-				training_dataset = pd.read_csv(training_file_path)
-				# Pick a random sample from the training dataset
-				training_dataset = training_dataset.sample(frac=training_dataset_size)
+	with tqdm(total=len(NEIGHBOURS_VALUES)*len(SPLITS)*len(TRAINING_DATASET_SIZE)) as progress_bar: # Create the progress bar
+		for neighbours_value in NEIGHBOURS_VALUES: # Loop through the neighbours values: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+			for x_split, y_split in SPLITS.items(): # Loop through the splits: 1x1, 2x2, 3x3 and 5x5.
+				for training_dataset_size in TRAINING_DATASET_SIZE: # Loop through the training dataset sizes
+					# The path for the training dataset file
+					training_file_path = f"{DATASETS_PATH['training']}/{x_split}x{y_split}-normalized-pixel_count{DATASET_FILES_FORMAT}"
+					# Load the training dataset
+					training_dataset = pd.read_csv(training_file_path)
+					# Pick a random sample from the training dataset
+					training_dataset = training_dataset.sample(frac=training_dataset_size)
 
-				# The path for the test dataset file
-				test_file_path = f"{DATASETS_PATH['test']}/{x_split}x{y_split}-normalized-pixel_count{DATASET_FILES_FORMAT}"
-				# Load the test dataset
-				test_dataset = pd.read_csv(test_file_path)
+					# The path for the test dataset file
+					test_file_path = f"{DATASETS_PATH['test']}/{x_split}x{y_split}-normalized-pixel_count{DATASET_FILES_FORMAT}"
+					# Load the test dataset
+					test_dataset = pd.read_csv(test_file_path)
 
-				# For every line in the test dataset
-				for index, test_dataset_row in tqdm(test_dataset.iterrows(), desc=f"{backgroundColors.CYAN}KNN: K={neighbours_value}, GRID={x_split}x{y_split} and Training Size={training_dataset_size}{Style.RESET_ALL}"):
-					for index, training_dataset_row in training_dataset.iterrows():
-						euclidean_distances = [] # The euclidean distances
-						# Calculate the euclidean distance between the test dataset row and the training dataset
-						for i in range(len(test_dataset_row.values)):
-							print(f"{backgroundColors.YELLOW}test_dataset_row.values: {test_dataset_row.values[i]}{Style.RESET_ALL}")
-							print(f"{backgroundColors.YELLOW}training_dataset_row.values: {training_dataset_row.values[i]}{Style.RESET_ALL}")
-							time.sleep(1)
-							eucledian_distance = ((float(test_dataset_row.values[i]) - float(training_dataset_row.values[i])) ** 2) ** 0.5
-						# Create a new column with the euclidean distances
-						training_dataset["euclidean_distance"] = euclidean_distances
-						# Sort the training dataset by the euclidean distance
-						training_dataset = training_dataset.sort_values(by=["euclidean_distance"])
-						# Get the first K rows from the training dataset
-						k_nearest_neighbours = training_dataset.head(neighbours_value)
-						# Get the most frequent label from the K nearest neighbours
-						most_frequent_label = k_nearest_neighbours["Digit Class"].mode()[0]
-						# Add the most frequent label to the test dataset
-						test_dataset.loc[index, "KNN"] = most_frequent_label
-						# Verify if the most frequent label is equal to the actual label (Digit Class) column
-						if most_frequent_label == test_dataset_row["Digit Class"]:
-							# Add 1 to the correct predictions column
-							results[neighbours_value][f"{x_split}x{y_split}"][training_dataset_size]["Correct Predictions"] += 1
-						else:
-							# Add 0 to the correct predictions column
-							results[neighbours_value][f"{x_split}x{y_split}"][training_dataset_size]["Correct Predictions"] += 0
-						# Add 1 to the total predictions column
-						results[neighbours_value][f"{x_split}x{y_split}"][training_dataset_size]["Total Predictions"] += 1
+					# For every line in the test dataset
+					for index, test_dataset_row in test_dataset.iterrows():
+						for index, training_dataset_row in training_dataset.iterrows():
+							euclidean_distances = [] # The euclidean distances
+							# Calculate the euclidean distance between the test dataset row and the training dataset
+							for i in range(3, len(test_dataset_row)):
+								print(f"{backgroundColors.YELLOW}test_dataset_row.values: {test_dataset_row.values[i]}{Style.RESET_ALL}")
+								print(f"{backgroundColors.YELLOW}training_dataset_row.values: {training_dataset_row.values[i]}{Style.RESET_ALL}")
+								print(f"{backgroundColors.YELLOW}euclidean_distances: {euclidean_distances}{Style.RESET_ALL}")
+								euclidean_distances.append((test_dataset_row.values[i] - training_dataset_row.values[i])**2)
+							# Create a new column with the euclidean distances
+							training_dataset["euclidean_distance"] = euclidean_distances
+							# Sort the training dataset by the euclidean distance
+							training_dataset = training_dataset.sort_values(by=["euclidean_distance"])
+							# Get the first K rows from the training dataset
+							k_nearest_neighbours = training_dataset.head(neighbours_value)
+							# Get the most frequent label from the K nearest neighbours
+							most_frequent_label = k_nearest_neighbours["Digit Class"].mode()[0]
+							# Add the most frequent label to the test dataset
+							test_dataset.loc[index, "KNN"] = most_frequent_label
+							# Verify if the most frequent label is equal to the actual label (Digit Class) column
+							if most_frequent_label == test_dataset_row["Digit Class"]:
+								# Add 1 to the correct predictions column
+								results[neighbours_value][f"{x_split}x{y_split}"][training_dataset_size]["Correct Predictions"] += 1
+							else:
+								# Add 0 to the correct predictions column
+								results[neighbours_value][f"{x_split}x{y_split}"][training_dataset_size]["Correct Predictions"] += 0
+							# Add 1 to the total predictions column
+							results[neighbours_value][f"{x_split}x{y_split}"][training_dataset_size]["Total Predictions"] += 1
+
+			# Update the progress bar
+			progress_bar.update(1)
 
 # @brief: The entry point of the program
 # @param: None
