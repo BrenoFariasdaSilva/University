bc5f175b5a41be8d711d7bb38c1c71d483f0588d
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-10-28 10:48:56 -0300

    REFACTOR: Updating the Artificial Intelligence K-NN Python Code

--- Artificial Intelligence/02 - Pratical Class - K-NN/main.py
+++ Artificial Intelligence/02 - Pratical Class - K-NN/main.py
@@ -1,34 +1,44 @@
-"""
-Example of how to use the k-NN classifier using the scikit-learn library.
-"""
+import numpy as np # Linear Algebra
+from sklearn.neighbors import KNeighborsClassifier # K-NN
+from sklearn.metrics import classification_report # Classification report
+from colorama import Style # For coloring the terminal
 
-import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import classification_report
-from sklearn.svm import SVC
+# Macros:
+class backgroundColors: # Colors for the terminal
+	CYAN = "\033[96m" # Cyan
+	GREEN = "\033[92m" # Green
+	YELLOW = "\033[93m" # Yellow
+	RED = "\033[91m" # Red
 
-data_sets = ["digits", "spam", "writer"]
-data_set = data_sets[0]
+DATA_SETS = ["digits", "spam", "writer"] # Data sets
 
-for data_set in data_sets:
-	# Load the data
-	tr = np.loadtxt(f"./dataset/{data_set}/train.txt");
-	ts = np.loadtxt(f"./dataset/{data_set}/test.txt");
-	y_test = ts[:,-1]
-	y_train = tr[:,-1]
-	X_train = tr[:, 1 : -1]
-	X_test = ts[:, 1 : -1]
-			
-	# k-NN classifier
-	# Documentation: https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
+# This is the main function
+def main():
+	for data_set in DATA_SETS: # Iterate over the data sets
+		# Load the data
+		train = np.loadtxt(f"./dataset/{data_set}/train.txt");
+		test = np.loadtxt(f"./dataset/{data_set}/test.txt");
 
-	neigh = KNeighborsClassifier(n_neighbors = 20, metric = "euclidean")
-	neigh.fit(X_train, y_train)
-	print(classification_report(y_test, neigh.predict(X_test)))
+		# Get the labels and features
+		test_label = test[:,-1] # Get the labels
+		train_label = train[:,-1] # Get the labels
+		train_features_values = train[:, 1 : -1] # Get the features
+		test_features_values = test[:, 1 : -1] # Get the features
 
-	# class_counts = np.bincount(y_train.astype("int64"))
+		# Print the data set name
+		print(f"{backgroundColors.GREEN}K-NN Classifier:{backgroundColors.CYAN}")
+		neigh = KNeighborsClassifier(n_neighbors = 20, metric = "euclidean") # Instantiate the classifier
+		neigh.fit(train_features_values, train_label) # Train the classifier
+		print(f"{classification_report(test_label, neigh.predict(test_features_values))}{Style.RESET_ALL}") # Print the classification report
 
-	# # Print the class counts
-	# for class_label, count in enumerate(class_counts):
-	# 	if count > 0:
-	# 		print(f"Classe {class_label}: {count} amostra(s).")
+		# # Get the class counts
+		# class_counts = np.bincount(test_label.astype("int64"))
+
+		# # Print the class counts
+		# for class_label, count in enumerate(class_counts):
+		# 	if count > 0: # If the class has samples
+		# 		print(f"Class {class_label}: {count} samples")
+
+# This is the standard boilerplate that calls the main() function.
+if __name__ == "__main__":
+	main() # Call the main function
