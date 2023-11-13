f8d41d4fc3cdd3cb072ccab9dfc1e010d67d00d7
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-11-13 09:46:33 -0300

    REFACTOR: Updating Artificial Intelligence K-Means Python Code

--- Artificial Intelligence/08 - K-Means/main.py
+++ Artificial Intelligence/08 - K-Means/main.py
@@ -3,21 +3,21 @@ import time # For getting the current time
 import numpy as np # Import numpy
 import os # Import os
 import platform # For getting the operating system name
-from colorama import Style  # For coloring the terminal
+from colorama import Style # For coloring the terminal
 from sklearn.cluster import KMeans # Import KMeans
 from sklearn.model_selection import train_test_split # Import train_test_split
 from sklearn.neighbors import KNeighborsClassifier # Import KNeighborsClassifier
 from sklearn.preprocessing import MinMaxScaler # Import MinMaxScaler
 
 # Macros:
-class BackgroundColors:  # Colors for the terminal
-   CYAN = "\033[96m"  # Cyan
-   GREEN = "\033[92m"  # Green
-   YELLOW = "\033[93m"  # Yellow
-   RED = "\033[91m"  # Red
-   BOLD = "\033[1m"  # Bold
-   UNDERLINE = "\033[4m"  # Underline
-   CLEAR_TERMINAL = "\033[H\033[J"  # Clear the terminal
+class BackgroundColors: # Colors for the terminal
+   CYAN = "\033[96m" # Cyan
+   GREEN = "\033[92m" # Green
+   YELLOW = "\033[93m" # Yellow
+   RED = "\033[91m" # Red
+   BOLD = "\033[1m" # Bold
+   UNDERLINE = "\033[4m" # Underline
+   CLEAR_TERMINAL = "\033[H\033[J" # Clear the terminal
 
 # Sound Constants:
 SOUND_COMMANDS = {"Darwin": "afplay", "Linux": "aplay", "Windows": "start"}
