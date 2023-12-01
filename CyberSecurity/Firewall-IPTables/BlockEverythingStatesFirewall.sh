ec2383d130e489e33364f5a2f165ea563fd7a007
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-12-01 19:25:19 -0300

    FEAT: Adding the Firewall Stats ShellScripts to CyberSecurity

---
+++ CyberSecurity/Firewall-IPTables/BlockEverythingStatesFirewall.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+echo "Cleaning Tables"
+iptables -F
+iptables -t nat -F
+iptables -t mangle -F
+
+echo "Setting Up Firewall Policies"
+
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -P FORWARD DROP
+
+echo "NAT Rules"
+iptables -t nat -A POSTROUTING -o eth2 -j MASQUERADE
+
+echo "LAN 1 Screened"
+iptables -A FORWARD -s 10.1.0.0/24 -o eth2 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -d 10.1.0.0/24 -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+echo "HOST 3 Server HTTP"
+iptables -A FORWARD -d 10.2.0.3 -p tcp --dport 80 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -s 10.2.0.3 -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+echo "HOST 4 HTTP and HTTPS Server Only" 
+iptables -A FORWARD -d 10.2.0.4 -p tcp --dport 80 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -d 10.2.0.4 -p tcp --dport 443 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -s 10.2.0.4 -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+echo "HOST 1 Can Access Firewall by SSH controlled by MAC Address"
+iptables -A INPUT -p tcp --dport 22 -m mac --mac-source  fe:61:6b:46:da:a0 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT
+
+echo "Ping Firewall 4 Times"
+iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit-burst 4 -j ACCEPT
+iptables -A OUTPUT -p icmp -j ACCEPT
---
+++ CyberSecurity/Firewall-IPTables/StatesFirewall.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+echo "Cleaning Tables"
+iptables -F
+iptables -t nat -F
+iptables -t mangle -F
+
+echo "Setting Up Firewall Policies"
+
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+
+echo "NAT Rules"
+iptables -t nat -A POSTROUTING -o eth2 -j MASQUERADE
+
+echo "LAN 10.1.0.0 Screened"
+iptables -A FORWARD -o eth1 -d 10.1.0.0/24 -m state --state NEW,INVALID -j DROP
+
+echo "Host 3 Server HTTP Only"
+iptables -A FORWARD -d 10.2.0.3/24 -p tcp ! --dport 80 -m state --state NEW,INVALID -j DROP
+
+echo "Host 4 Server HTTP and HTTPS Only"
+iptables -A FORWARD -d 10.2.0.4 -p tcp --dport 80 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -d 10.2.0.4 -p tcp --dport 443 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -d 10.2.0.4 -m state --state NEW,INVALID -j DROP
